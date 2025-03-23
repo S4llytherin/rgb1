895dfde8007adfa611cff8307a7ab2a399f96715
S4llytherin <salihtunahanbasol@gmail.com>
2025-03-23 22:13:01 +0300

    Add files via upload

--- rgb.php
+++ rgb.php
@@ -1,51 +1,32 @@
-<!DOCTYPE html>
-<html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Çarpım Tablosu</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            margin: 20px;
-        }
-        td {
-            padding: 10px;
-            font-size: 20px;
-            text-align: center;
-            border: 1px solid black;
-        }
-    </style>
-</head>
-<body>
+<?php
+session_start();
 
-    <?php
-    if (isset($_GET["sayi1"]) && isset($_GET["sayi2"])) {
-        $sayi1 = intval($_GET["sayi1"]);
-        $sayi2 = intval($_GET["sayi2"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sayi1"]) && isset($_POST["sayi2"])) {
+    $sayi1 = intval($_POST["sayi1"]);
+    $sayi2 = intval($_POST["sayi2"]);
 
-        if ($sayi1 < 1 || $sayi2 < 1) {
-            echo "<p>Lütfen 1 veya daha büyük sayılar girin.</p>";
-        } else {
-            echo "<table>";
-            for ($i = 1; $i <= $sayi1; $i++) {
-                echo "<tr>";
-                for ($j = 1; $j <= $sayi2; $j++) {
-                    $r = rand(0, 255);
-                    $g = rand(0, 255);
-                    $b = rand(0, 255);
-                    echo "<td style='background-color: rgb($r, $g, $b);'>";
-                    echo "$i × $j = " . ($i * $j);
-                    echo "</td>";
-                }
-                echo "</tr>";
-            }
-            echo "</table>";
-        }
+    if ($sayi1 < 1 || $sayi2 < 1) {
+        $_SESSION["sonuc"] = "<p>Lütfen 1 veya daha büyük sayılar girin.</p>";
     } else {
-        echo "<p>Lütfen giriş sayfasından veri girin.</p>";
-    }
-    ?>
+        $tablo = "<table>";
+        for ($i = 1; $i <= $sayi1; $i++) {
+            $tablo .= "<tr>";
+            for ($j = 1; $j <= $sayi2; $j++) {
+                $r = rand(0, 255);
+                $g = rand(0, 255);
+                $b = rand(0, 255);
+                $tablo .= "<td style='background-color: rgb($r, $g, $b);'>";
+                $tablo .= "$i × $j = " . ($i * $j);
+                $tablo .= "</td>";
+            }
+            $tablo .= "</tr>";
+        }
+        $tablo .= "</table>";
 
-</body>
-</html>
+        $_SESSION["sonuc"] = $tablo;
+    }
+}
+
+header("Location: index.html");
+exit();
+?>
